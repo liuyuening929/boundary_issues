ef5d7cb236da556c79e00032ae8e08a5a249b9e4
Erika <emk2237@columbia.edu>
2025-09-02 18:58:37 +0000

    made a new tensorboard script to run from the terminal when running the training

---
+++ scripts/tensorboard.py
@@ -0,0 +1,18 @@
+import subprocess
+
+def find_free_port():
+    import socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("", 0))
+        return s.getsockname()[1]
+def launch_tensorboard(log_dir):
+    port = find_free_port()
+    tensorboard_cmd = f"tensorboard --logdir={log_dir} --port={port}"
+    process = subprocess.Popen(tensorboard_cmd, shell=True)
+    print(
+        f"TensorBoard started at http://localhost:{port}. \n"
+        "If you are using VSCode remote session, forward the port using the PORTS tab next to TERMINAL."
+    )
+    return process
+
+tensorboard_process = launch_tensorboard("training_logs")
